3046e7132342c606c08f99d96faddb14c6c01500
vasu-kadari4748 <vksrvksr98@gmail.com>
2023-06-20 22:25:22 +0530

    Updates

---
+++ precache-manifest.8c71bffa85ffcd0c67be081d054beef2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a08b407b7ea97e521242a8ebc7f17ff",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "90422fe2d622b6fa3f82",
+    "url": "/Keeper-App/static/js/2.d4766c91.chunk.js"
+  },
+  {
+    "revision": "11268bdd0982cdee223b",
+    "url": "/Keeper-App/static/js/main.428f37a5.chunk.js"
+  },
+  {
+    "revision": "4c52c3fd6a1d03a0311b",
+    "url": "/Keeper-App/static/js/runtime-main.0f673c37.js"
+  }
+]);
